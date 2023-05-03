70450d3b2d8f1ca0b9db360c5cddf9061dfed760
rrdmp15 <rdayismar@gmail.com>
2023-05-03 09:32:27 -0500

    feat: :rocket: Webcomponents

---
+++ app.js
@@ -0,0 +1,5 @@
+import myHeader from "./components/my-header.js";
+import mySection from "./components/my-section.js";
+import myNav from "./components/my-nav.js";
+import mySeleccion from "./components/my-seleccion.js";
+import myFooter from "./components/my-footer.js";
---
+++ components/my-header.js
@@ -0,0 +1,18 @@
+let pathName = new URL (import.meta.url).pathname;
+let name = pathName.split("/").pop().replace(".js","");
+
+export default class myHeader extends HTMLElement{
+   static async components(){
+    return await (await fetch(pathName.replace(".js",".html"))).text();
+   }
+
+   constructor(){
+    super();
+    this.attachShadow({mode: "open"});
+    Promise.resolve(myHeader.components()).then(html=>{
+        this.shadowRoot.innerHTML = html;
+    })
+   }
+ 
+}
+customElements.define(name, myHeader)
